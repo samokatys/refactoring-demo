f30bd3dc189bcf93551f61b311de2aa5ead90aca
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-08-08 12:34:36 +0300

    Add stub & mock Demo.

--- src/test/java/com/scrumtrek/simplestore/demo/ArrayListTest.java
+++ src/test/java/com/scrumtrek/simplestore/demo/ArrayListTest.java
@@ -9,8 +9,12 @@ import java.util.ArrayList;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assume.assumeTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 public class ArrayListTest {
+    @Ignore
     @Test(timeout = 1_000)
     public void shouldSizeIncrementedWhenElementAdd() { //BDD = DDD + tests
         //region Fixture | Arrange | Given
@@ -39,5 +43,43 @@ public class ArrayListTest {
     public void shouldGetErrorWhenElementIsNull() {
         ArrayList<Object> sut = new ArrayList<>();
         sut.add(null);
+    }
+
+    @Test
+    public void shouldToStringDependsOnElementsWhenStubToString() {
+        //region Given
+        ArrayList<Object> sut = new ArrayList<>();
+        String stub = mock(String.class);
+        when(stub.toString())
+                .thenReturn("test string 1")
+                .thenReturn("test string 2");
+
+        /*
+        when(stub.equals("1")).thenReturn(false);
+        when(stub.equals("2")).thenReturn(true);
+        when(stub.equals(anyString())).thenReturn(false);
+        when(stub.equals(any(MyClass.class))).thenReturn(false);
+        */
+        sut.add(stub);
+        //endregion
+
+        //region When
+        String result = sut.toString();
+        //endregion
+
+        //region Then
+        assertThat(result).contains("test string");
+        //endregion
+    }
+
+    @Test
+    public void shouldToStringDependsOnElementsWhenMockToString() {
+        ArrayList<Object> sut = new ArrayList<>();
+        Object mock = mock(Object.class);
+        sut.add(mock);
+
+        sut.toString();
+
+        verify(mock, times(1)).toString();
     }
 }
